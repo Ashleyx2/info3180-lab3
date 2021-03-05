98c266576e38f340103f4f19a01b9374b0b12756
unknown <ashleysquared99@gmail.com>
2021-03-05 00:54:00 -0500

    Completed lab

--- app/views.py
+++ app/views.py
@@ -7,11 +7,15 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from .forms import ContactForm
+from app import mail
+from flask_mail import Mail, Message
 
 ###
 # Routing for your application.
 ###
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
@@ -24,6 +28,20 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    """Render the website's contact page."""
+    form = ContactForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        msg = Message(request.form['subject'], sender=(request.form['name'], request.form['email']), recipients=["b027203e8e-ae964c@inbox.mailtrap.io"])
+        msg.body = request.form['message']
+        mail.send(msg)
+        flash('Your email has been sent.', 'success')
+        return redirect(url_for('home'))
+
+    flash_errors(form)
+    return render_template('contact.html', form=form, template="form-template")
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -57,7 +75,6 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
-
 
 @app.errorhandler(404)
 def page_not_found(error):
